433c9115299297c413c4c93b48449e534f421f54
Jillurcf <jrtushar@gmail.com>
2026-06-05 00:56:32 +0600

    .env created at the project root level and superbase url and anon key added

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,47 @@
+// import { initDB } from "@/database/initDB";
+// import { Stack } from "expo-router";
+// import { useEffect } from "react";
+
+// export default function RootLayout() {
+
+//   useEffect(() => {
+//   initDB();
+// }, []);
+//   return (
+//     <Stack
+//       screenOptions={{
+//         headerShown: false,
+//       }}
+//     />
+//   );
+// }
+
 import { initDB } from "@/database/initDB";
 import { Stack } from "expo-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
 
 export default function RootLayout() {
+  const [ready, setReady] = useState(false);
 
   useEffect(() => {
-  initDB();
-}, []);
+    const setup = async () => {
+      await initDB();
+      setReady(true);
+    };
+
+    setup();
+  }, []);
+
+  if (!ready) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <Stack screenOptions={{ headerShown: false }} />
   );
 }
